92bb5c9fb79a595ec7b5160fc5ba8023565acbdd
Michael <mraypold@gmail.com>
2015-03-03 16:32:29 -0700

    Cleaned up URLs

--- author/urls.py
+++ author/urls.py
@@ -5,34 +5,33 @@ from author import views, viewsauth
 urlpatterns = [
 
     # GET  /author/:uuid
-    url(r'^author/(?P<uuid>[0-9a-zA-Z]{32}\Z)$', views.GetUserDetails.as_view(),
+    url(r'^(?P<uuid>[0-9a-zA-Z]{32}\Z)$', views.GetUserDetails.as_view(),
         name = 'user_details'),
 
     # GET /author/friends/:uuid
-    url(r'^author/friends/(?P<uuid>[0-9a-zA-Z]{32}\Z)$', views.GetAuthorFriends.as_view(),
+    url(r'^friends/(?P<uuid>[0-9a-zA-Z]{32}\Z)$', views.GetAuthorFriends.as_view(),
         name = 'user_friends'),
 
     # GET /author/followers/:uuid
-    url(r'^author/followers/(?P<uuid>[0-9a-zA-Z]{32}\Z)$', views.GetAuthorFollowers.as_view(),
+    url(r'^followers/(?P<uuid>[0-9a-zA-Z]{32}\Z)$', views.GetAuthorFollowers.as_view(),
         name = 'user_followers'),
 
     # GET /author/friendrequests/:uuid
-    url(r'^author/friendrequests/(?P<uuid>[0-9a-fA-Z]{32}\Z)$', views.GetAuthorFriendRequests.as_view(),
+    url(r'^friendrequests/(?P<uuid>[0-9a-fA-Z]{32}\Z)$', views.GetAuthorFriendRequests.as_view(),
         name = 'user_friend_requests'),
 
     # POST /author/registration/
-    url(r'^author/registration/$', viewsauth.AuthorRegistration,
+    url(r'^registration/$', viewsauth.AuthorRegistration,
         name = 'registration'),
 
     # POST /login
-    url(r'^author/login/$', viewsauth.Login.as_view(), name='login'),
+    url(r'^login/$', viewsauth.Login.as_view(), name='login'),
 
     # POST /logout
-    url(r'^author/logout/$', viewsauth.Logout.as_view(), name='logout'),
-
+    url(r'^logout/$', viewsauth.Logout.as_view(), name='logout'),
 
    # GET /author/getid/:username
-    url(r'^author/getid/(?P<username>[0-9a-zA-Z]+)$', viewsauth.GetUserUUID,
+    url(r'^getid/(?P<username>[0-9a-zA-Z]+)$', viewsauth.GetUserUUID,
         name = 'user_uuid'),
 ]
 
