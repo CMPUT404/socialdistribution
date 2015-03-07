724ba10082c594012fab69aca07550643690b9dc
Jonathan Cairo <jcairo@users.noreply.github.com>
2015-03-07 16:42:37 -0700

    Added ability to follow, unfollow, and befriend.

--- author/views.py
+++ author/views.py
@@ -1,7 +1,8 @@
+import json
 from backend.utils import UsernameNotFound
-
 from django.contrib.auth.models import User
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import generics
@@ -80,7 +81,7 @@ class GetAuthorFriends(ListAPIView):
             raise UsernameNotFound()
 
 # GET /author/followers/:username
-class GetAuthorFollowers(ListAPIView):
+class GetAuthorFollowers(APIView):
     """
     Returns a JSON object containing a followee and a list of followers
 
@@ -93,10 +94,10 @@ class GetAuthorFollowers(ListAPIView):
             ]
         }
     """
-
-    def list(self, request, *args, **kwargs):
+    authentication_classes = (TokenAuthentication,)
+    # permission_classes = (IsAuthenticated,)
+    def get(self, request, *args, **kwargs):
         user = get_user(kwargs['username'])
-
         if user:
             # Retrieve only the usernames of the followers for a given user
             followers = FollowerRelationship.objects.filter(followee = user)\
@@ -106,6 +107,46 @@ class GetAuthorFollowers(ListAPIView):
             return Response({'followee':user.username, 'followers':relations})
         else:
             raise UsernameNotFound()
+
+    def post(self, request, username, format=None):
+        followee = get_user(username)
+        new_follower = User.objects.get(username=request.POST.get('follower', ""))
+        if followee == None:
+            return Response("Followee not found", status=status.HTTP_404_NOT_FOUND)
+        if new_follower == None:
+            return Response("Follower not found", status=status.HTTP_404_NOT_FOUND)
+        # check if the followee also follows the new follower
+        # if so create a new friend relationship and remove both foller
+        # relationships
+        if FollowerRelationship.objects.filter(follower=followee, followee=new_follower).count():
+            FriendRelationship.objects.create(friendor=new_follower, friend=followee)
+            FriendRelationship.objects.create(friendor=followee, friend=new_follower)
+            FollowerRelationship.objects.get(follower=followee, followee=new_follower).delete()
+            return Response({"friendor":new_follower.username, "friend":followee.username}, status=status.HTTP_201_CREATED)
+
+        # otherwise create new follower relationship
+        FollowerRelationship.objects.create(follower=new_follower, followee=followee)
+        return Response({"follower":new_follower.username, "followee":followee.username}, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, username, format=None):
+        # unfollow
+        followee = get_user(username)
+        formattted_request = str(request.body).strip("'<>()[]\"` ").replace('\'', '\"')
+        new_unfollower = json.loads(formattted_request)['follower']
+        new_unfollower = User.objects.get(username=new_unfollower)
+
+        # check whether the users are friends
+        if FriendRelationship.objects.filter(friendor=followee, friend=new_unfollower).count():
+            # remove the friend relationship and restore the following
+            # relationship.
+            FriendRelationship.objects.get(friendor=followee, friend=new_unfollower).delete()
+            FriendRelationship.objects.get(friendor=new_unfollower, friend=followee).delete()
+            FollowerRelationship.objects.create(follower=followee, followee=new_unfollower)
+            return Response(status=status.HTTP_200_OK)
+
+        # if the users aren't friends just remove the follower relationship
+        FollowerRelationship.objects.get(follower=new_unfollower, followee=followee).delete()
+        return Response(status=status.HTTP_200_OK)
 
 # GET /author/friendrequests/:username
 class GetAuthorFriendRequests(ListAPIView):
