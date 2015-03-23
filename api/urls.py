22ad2532841025eb60a7ed3fef874653e02c42df
derekdowling <me@derekdowling.com>
2015-03-23 15:30:48 -0600

    Cleaning up custom urls imports

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from views.content import PostViewSet, AuthorPostViewSet, PublicPostsViewSet
 from rest_framework_nested import routers
-from custom_urls import content_urls
+from custom_urls import content as ContentUrls
 
 router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'author', AuthorPostViewSet)
@@ -15,7 +15,7 @@ author_router.register(r'posts', AuthorPostViewSet)
 urlpatterns = patterns('',
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^post', include(content_urls)),
+    url(r'^post', include(ContentUrls)),
     url(r'^', include('author_api.urls')),
     url(r'^', include(router.urls)),
     url(r'^', include(author_router.urls))
