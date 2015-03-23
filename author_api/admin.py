ee8d95b26afa30402d6f4b185b884bd655b7f9ff
Michael Raypold <mraypold@users.noreply.github.com>
2015-03-22 20:13:53 -0600

    Some fixes for admin panel/pep8 changes

--- author_api/admin.py
+++ author_api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from models import Author
-from models import FollowerRelationship, FriendRelationship, FriendRequest
+from models import Author, CachedAuthor
+
 
 def get_fields(model):
     """Returns a list of model fields that do not contain _id
@@ -11,21 +11,12 @@ def get_fields(model):
     Django returns fields in alpabetical order, so list is alphabetized.
     """
     return filter(lambda field: not field.endswith('_id'),
-        model._meta.get_all_field_names())
+                  model._meta.get_all_field_names())
+
 
 class AuthorOptions(admin.ModelAdmin):
     list_display = ['id', 'user', 'github_username', 'host', 'bio']
 
-class FollowerOptions(admin.ModelAdmin):
-    list_display = get_fields(FollowerRelationship)
-
-class FriendOptions(admin.ModelAdmin):
-    list_display = get_fields(FriendRelationship)
-
-class FriendRequestOptions(admin.ModelAdmin):
-    list_display = get_fields(FriendRequest)
 
 admin.site.register(Author, AuthorOptions)
-admin.site.register(FollowerRelationship, FollowerOptions)
-admin.site.register(FriendRelationship, FriendOptions)
-admin.site.register(FriendRequest, FriendRequestOptions)
+admin.site.register(CachedAuthor)
