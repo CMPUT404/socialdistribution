6d18fdf547c7c72a21dd83beb4d393fe6d210a31
Michael Raypold <mraypold@users.noreply.github.com>
2015-03-23 09:51:03 -0600

    Signals for Author/CachedAuthor

--- author_api/admin.py
+++ author_api/admin.py
@@ -2,21 +2,17 @@ from django.contrib import admin
 from models import Author, CachedAuthor
 
 
-def get_fields(model):
-    """Returns a list of model fields that do not contain _id
-
-    Takes a django model instance.
-    Fields containing _id are not representable in the admin interface.
-
-    Django returns fields in alpabetical order, so list is alphabetized.
-    """
-    return filter(lambda field: not field.endswith('_id'),
-                  model._meta.get_all_field_names())
-
-
 class AuthorOptions(admin.ModelAdmin):
     list_display = ['id', 'user', 'github_username', 'host', 'bio']
 
 
+class CachedAuthorOptions(admin.ModelAdmin):
+    list_display = ['id', 'displayname', 'host', 'url']
+
+    # Deletion should occur only through Author models and friend/followers
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
 admin.site.register(Author, AuthorOptions)
-admin.site.register(CachedAuthor)
+admin.site.register(CachedAuthor, CachedAuthorOptions)
