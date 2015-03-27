5a3dc53d4f770cce03e9016f557e540735edf988
Karim Baaba <karim.baaba@gmail.com>
2015-03-27 04:05:41 -0600

    Gulp Builds!

--- app/scripts/routes.jsx
+++ app/scripts/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { default as Router, Route, DefaultRoute } from 'react-router';
+import { Route, DefaultRoute, NotFoundRoute } from 'react-router';
 
 import Layout from './components/routes/layout';
 import Timeline from './components/routes/timeline';
@@ -7,22 +7,15 @@ import Author from './components/routes/author';
 import Login from './components/routes/login';
 import Register from './components/routes/register';
 import Posts from './components/routes/posts';
+import NotFound from './components/routes/not-found';
 
-var routes = (
+export default (
 	<Route path="/" name="layout" handler={Layout}>
 		<DefaultRoute name="timeline" handler={Timeline} />
 		<Route name="author" path="/author/:id/:host?" handler={Author} />
 		<Route name="login" handler={Login} />
 		<Route name="register" handler={Register} />
 		<Route name="posts" handler={Posts} />
+		<NotFoundRoute handler={NotFound} />
 	</Route>
 );
-
-// Don't touch this, define routes above
-export function start() {
-	// TODO: Use Router.HistoryLocation for prod
-	// Gulp webserver doesn't support this properly for dev
-  Router.run(routes, function (Handler) {
-		React.render(<Handler />, document.getElementById('app'));
-	});
-}
