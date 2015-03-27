f7068cdd0cd9a22f55f40fdd534c1702e0a505aa
Karim Baaba <karim.baaba@gmail.com>
2015-03-27 07:16:38 -0600

    Fix bug from #171 and close #102

--- api/serializers/author.py
+++ api/serializers/author.py
@@ -6,12 +6,14 @@ from ..models.author import (
     CachedAuthor
 )
 
+
 class AuthorImageSerializer(ImageSerializer):
     def to_representation(self, data):
         if data:
             return '/author/profile/images/' + data.name.split('/')[-1]
         else:
             return ''
+
 
 class AuthorUpdateSerializer(serializers.Serializer):
     """
@@ -20,12 +22,12 @@ class AuthorUpdateSerializer(serializers.Serializer):
     Update passwords is done separately.
     Username cannot be updated.
     """
-    email = serializers.EmailField(required = False)
-    bio = serializers.CharField(required = False)
-    first_name = serializers.CharField(required = False)
-    last_name = serializers.CharField(required = False)
-    github_username = serializers.CharField(required = False)
-    image = AuthorImageSerializer(required = False)
+    email = serializers.EmailField(required=False)
+    bio = serializers.CharField(required=False)
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+    github_username = serializers.CharField(required=False)
+    image = AuthorImageSerializer(required=False)
 
     def update(self, instance, validated_data):
         """
@@ -36,20 +38,22 @@ class AuthorUpdateSerializer(serializers.Serializer):
             validated_dated: Scrubed data from an HTTP request.
         """
         instance.bio = validated_data.get('bio', instance.bio)
-        instance.github_username = validated_data.get('github_username', \
-        instance.github_username)
+        instance.github_username = validated_data.get('github_username',
+                                                      instance.github_username)
+
         instance.image = validated_data.get('image', instance.image)
         instance.save()
 
         instance.user.email = validated_data.get('email', instance.user.email)
-        instance.user.last_name = validated_data.get('last_name', \
-            instance.user.last_name)
-        instance.user.first_name = validated_data.get('first_name', \
-            instance.user.first_name)
+        instance.user.last_name = validated_data.get('last_name',
+                                                     instance.user.last_name)
+        instance.user.first_name = validated_data.get('first_name',
+                                                      instance.user.first_name)
 
         instance.user.save()
 
         return instance
+
 
 class RegistrationSerializer(serializers.Serializer):
     """
@@ -60,18 +64,18 @@ class RegistrationSerializer(serializers.Serializer):
         Call .create() to build/insert models after validation.
     """
     displayname = serializers.CharField()
-    email = serializers.EmailField(required = False)
+    email = serializers.EmailField(required=False)
     password = serializers.CharField()
-    bio = serializers.CharField(required = False)
-    first_name = serializers.CharField(required = False)
-    last_name = serializers.CharField(required = False)
-    github_username = serializers.CharField(required = False)
-    image = AuthorImageSerializer(required = False)
+    bio = serializers.CharField(required=False)
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+    github_username = serializers.CharField(required=False)
+    image = AuthorImageSerializer(required=False)
 
     # Follow the same pattern to validate other fields if you desire.
     def validate_displayname(self, value):
         """Check if user exists"""
-        if User.objects.filter(username = value):
+        if User.objects.filter(username=value):
             raise serializers.ValidationError("Displayname already exists")
         return value
 
@@ -85,17 +89,16 @@ class RegistrationSerializer(serializers.Serializer):
         _author = {}
         _author['github_username'] = validated_data.pop('github_username', '')
         _author['bio'] = validated_data.pop('bio', '')
-        _author['image'] = validated_data.pop('image','')
-
-        #TODO: set host
+        _author['image'] = validated_data.pop('image', '')
 
         user = User.objects.create_user(**validated_data)
         user.save()
 
-        author = Author(user = user, **_author)
+        author = Author(user=user, **_author)
         author.save()
 
         return author
+
 
 class CompactAuthorSerializer(serializers.ModelSerializer):
     displayname = serializers.CharField(source='user.username')
@@ -104,17 +107,20 @@ class CompactAuthorSerializer(serializers.ModelSerializer):
         model = Author
         fields = ('id', 'displayname', 'host', 'url')
 
+
 # This will throw an error if duplicate id's
 class CachedAuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = CachedAuthor
         fields = ('id', 'host', 'displayname', 'url',)
 
-# This will not throw an error if duplicate id's and istead return existing model
+
+# This will not throw an error if duplicate id's
+# Instead returns an existing model
 class DirtyCachedAuthorSerializer(serializers.Serializer):
-    id = serializers.UUIDField(required = True)
-    host = serializers.URLField(required = True)
-    displayname = serializers.CharField(required = True)
+    id = serializers.UUIDField(required=True)
+    host = serializers.URLField(required=True)
+    displayname = serializers.CharField(required=True)
     url = serializers.URLField(required=False)
 
     def to_internal_value(self, data):
@@ -129,7 +135,7 @@ class DirtyCachedAuthorSerializer(serializers.Serializer):
         This is done, as foreign node CachedAuthors may not already exist.
         """
         try:
-            existing = CachedAuthor.objects.get(id = validated_data['id'])
+            existing = CachedAuthor.objects.get(id=validated_data['id'])
             return self.update(existing, validated_data)
         except:
             return self.create(validated_data)
@@ -139,70 +145,80 @@ class DirtyCachedAuthorSerializer(serializers.Serializer):
         cached.save()
         return cached
 
+
 class BaseRetrieveFollowersSerializer(serializers.ModelSerializer):
-    followers = CachedAuthorSerializer(many = True)
+    followers = CachedAuthorSerializer(many=True)
 
     class Meta:
         model = Author
         fields = ('followers',)
 
+
 class BaseRetrieveFriendsSerializer(serializers.ModelSerializer):
-    friends = CachedAuthorSerializer(many = True)
+    friends = CachedAuthorSerializer(many=True)
 
     class Meta:
         model = Author
         fields = ('friends',)
 
+
 class BaseRetrieveFollowingSerializer(serializers.ModelSerializer):
-    following = CachedAuthorSerializer(many = True)
+    following = CachedAuthorSerializer(many=True)
 
     class Meta:
         model = Author
         fields = ('following',)
 
+
 class RetrieveFollowersSerializer(BaseRetrieveFollowersSerializer):
     """Provideds only a list of the follower guids in the return object"""
     followers = serializers.StringRelatedField(many=True)
+
 
 class RetrieveFriendsSerializer(BaseRetrieveFriendsSerializer):
     """Provides only a list of the friend guids in the return object"""
     friends = serializers.StringRelatedField(many=True)
 
+
 class AuthorRelationSerializer(serializers.ModelSerializer):
     """writable serializer for followers and friends only"""
-    followers = CachedAuthorSerializer(many = True)
-    friends = CachedAuthorSerializer(many = True)
-    following = CachedAuthorSerializer(many = True)
+    followers = CachedAuthorSerializer(many=True)
+    friends = CachedAuthorSerializer(many=True)
+    following = CachedAuthorSerializer(many=True)
 
     class Meta:
         model = Author
         fields = ('followers', 'friends', 'following')
-        read_only_fields = ('user', 'id', 'host', 'bio', 'github_username', \
-            'image',)
+        read_only_fields = ('user', 'id', 'host', 'bio', 'github_username',
+                            'image',)
+
 
 class AuthorSerializer(serializers.ModelSerializer):
     displayname = serializers.CharField(source='user.username')
-    email       = serializers.EmailField(source='user.email')
-    first_name  = serializers.CharField(source='user.first_name')
-    last_name   = serializers.CharField(source='user.last_name')
-    image       = AuthorImageSerializer(required = False)
-    friends     = CachedAuthorSerializer(many = True)
-    following   = CachedAuthorSerializer(many = True)
-    pending     = CachedAuthorSerializer(many = True)
-    requests    = CachedAuthorSerializer(many = True)
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
+    email = serializers.EmailField(source='user.email')
+    image = AuthorImageSerializer(required=False)
+    following = CachedAuthorSerializer(many=True)
+    requests = CachedAuthorSerializer(many=True)
+    friends = CachedAuthorSerializer(many=True)
+    pending = CachedAuthorSerializer(many=True)
 
     class Meta:
         model = Author
-        fields = ('id', 'displayname', 'email', 'first_name', 'last_name', \
-            'github_username', 'bio', 'host', 'url', 'image', 'friends', 'following', 'pending', 'requests')
+        fields = ('id', 'displayname', 'email', 'first_name', 'last_name',
+                  'github_username', 'bio', 'host', 'url', 'image', 'friends',
+                  'following', 'pending', 'requests')
 
     def to_representation(self, obj):
         representation = super(AuthorSerializer, self).to_representation(obj)
 
-        if (not hasattr(self.context, 'request')) or (self.context.request.user.id != obj.user.id):
-            representation.pop("following", None)
-            representation.pop("pending", None)
-            representation.pop("requests", None)
+        if ('request' not in self.context) or \
+           (self.context['request'].user.id != obj.user.id):
+            representation["following"] = []
+            representation["requests"] = []
+            representation["pending"] = []
+
         return representation
 
 
